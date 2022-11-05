5926bd6f52db356c42d597baeafc43fa54acb7ee
Alex Stone <stone@filamentgames.com>
2022-11-05 00:28:44 -0500

    drive sequence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import sys
-import asyncio
+from asyncio import set_event_loop_policy, run, gather, sleep, WindowsSelectorEventLoopPolicy
 if (sys.path[len(sys.path)-1] != "C:\\Users\\ajstone\\Documents\\My Games\\Roboco\\st0nerhat\\Scripts"):
     sys.path.append("C:\\Users\\ajstone\\Documents\\My Games\\Roboco\\st0nerhat\\Scripts")
 import importlib
@@ -14,16 +14,27 @@ from framework.core import UpdateLoop
 from local.input import *
 from local.output import *
 
+async def dance():
+    drive.forward(10)
+    await sleep(3)
+    drive.pivotRight(10)
+    await sleep(3)
+    drive.stop()
+    await sleep(2)
+    drive.reverse(10)
+    await sleep(2)
+    drive.stop()
 
 async def main():
     loop = UpdateLoop(0.033, 0.015)
     loop.add(leftLed)
     loop.add(rightLed)
-   
-    drive.forwardLeft(5)
+    
+    await gather(
+        loop.run(),
+        dance()
+    )
 
-    await loop.run()
-
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-asyncio.run(main())
+set_event_loop_policy(WindowsSelectorEventLoopPolicy())
+run(main())
 
